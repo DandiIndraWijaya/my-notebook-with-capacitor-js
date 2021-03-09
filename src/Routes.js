c9babdfda4ec13ef3b89946b0fc820545b6aa81a
Dandi Indra <dandiindra29@gmail.com>
2021-03-09 10:23:45 +0700

    Change page name from Schedules to Todos

--- src/Routes.js
+++ src/Routes.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Route, useLocation } from 'react-router-dom';
 import SlideRoutes from 'react-slide-routes';
 import Index from './pages/Index/Index';
-import Schedules from './pages/Schedules/Schedules';
+import Todos from './pages/Todos/Todos';
 
 const Routes = () => {
   const location = useLocation();
   return (
     <SlideRoutes
-      pathList={['schedules', '/']}
+      pathList={['todos', '/']}
       location={location}
       duration={500}
     >
       <Route exact path="/" component={Index} />
-      <Route path="/schedules" component={Schedules} />
+      <Route path="/todos" component={Todos} />
     </SlideRoutes>
   );
 };
--- src/pages/Todos/Todos.jsx
+++ src/pages/Todos/Todos.jsx
@@ -2,24 +2,24 @@ import React, { useState } from 'react';
 import { withTheme } from '@emotion/react';
 import { withRouter } from 'react-router-dom';
 import TimeRangePicker from '@wojtekmaj/react-timerange-picker';
-import styles from './Schedules.module.css';
+import styles from './Todos.module.css';
 import Button from '../../components/Button/Button';
 
-const Schedules = () => {
+const Todos = () => {
   const [time, setTime] = useState(['10:00', '11:00']);
   const [add, setAdd] = useState(false);
-  const [buttonText, setButtonText] = useState('New');
+  const [buttonText, setButtonText] = useState('Add Todo');
   const onclickNewButton = () => {
     const condiditon = !add;
     setAdd(condiditon);
-    const text = !add ? 'Done' : 'New';
+    const text = !add ? 'Done' : 'Add Todo';
     setButtonText(text);
   };
 
   return (
     <div id="schedules" className={styles.schedules}>
       <center>
-        <h1>Schedules</h1>
+        <h1>Todos</h1>
       </center>
       <Button onClick={onclickNewButton}>{buttonText}</Button>
       <br />
@@ -49,4 +49,4 @@ const Schedules = () => {
   );
 };
 
-export default withTheme(withRouter(Schedules));
+export default withTheme(withRouter(Todos));
