da20f05b1e034cb07f143aff3f7d53784d2f7bcd
Dandi Indra <dandiindra29@gmail.com>
2021-03-12 08:07:57 +0700

    Chenge redirect onload

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -30,12 +30,6 @@ const Index = () => {
     location.reload();
   };
 
-  window.onload = () => {
-    if (user.name !== '') {
-      window.location.href = '/todos';
-    }
-  };
-
   useEffect(() => {
     if (name !== user.name || quote !== user.quote) {
       setIsEqual(true);
